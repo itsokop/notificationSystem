c5778b864f8b7a4dd7eb271e622300a0ede8c3a1
Om Prakash <114151590+itsokop@users.noreply.github.com>
2024-02-19 16:27:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import user from './routes.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    'origin': ["http://127.0.0.1:5500"]
+}))
+app.use(bodyParser.json())
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.json({"Hi":"Hello World"})
+})
+
+app.use('/api', user)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Port listening on ${PORT}`)
+})
+
